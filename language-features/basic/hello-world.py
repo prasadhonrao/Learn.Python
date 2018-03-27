de350ec35c62d408a4e0c6f543b366675111eba1
Prasad Honrao <Honrao.Prasad@Gmail.com>
2018-03-27 15:24:49 +0530

    Refactor code

--- language-features/basic/hello-world.py
+++ language-features/basic/hello-world.py
@@ -1,3 +1,10 @@
-print("Hello World")
+print("Hello, I'm Python!")
 
-print("Hello", "World", "Lets" , "Learn", "Python")
+print("Hello", "World", "Lets", "Learn", "Python")
+
+# Execute below code from command prompt
+name = input('Enter your name : ')
+print('You have entered : %s' % name)
+print('1. Hi {}'.format(name))
+print('2. Hi {0}'.format(name))
+print(f'3. Hi {name}')
