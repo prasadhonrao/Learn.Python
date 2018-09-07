186c43a1a9d500a1e9a6e9c31e757dae64e9d2a9
Prasad Honrao <Honrao.Prasad@Gmail.com>
2018-09-07 16:25:36 +0530

    Refactor code

--- language-features/basic/hello-world.py
+++ language-features/basic/hello-world.py
@@ -8,7 +8,5 @@ user_name = input('Enter your name : ')
 # print('You have entered : %s' % user_name)
 
 print('1. Hi {}'.format(user_name))
-
 print('2. Hi {0}'.format(user_name))
-
 print(f'3. Hi {user_name}')
