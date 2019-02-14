afaf3a783b2ccf4c8ec803c37db2efd85b7bc583
Prasad Honrao <Honrao.Prasad@Gmail.com>
2019-02-14 17:34:31 +0530

    Code refactor

--- language-features/collection/list/list.py
+++ language-features/collection/list/list.py
@@ -22,3 +22,7 @@ print(' ')
 
 # list length
 print("number of chars in PYTHON : {0}".format(len(list("PYTHON"))))
+
+# create a list from string
+s = "Python is one of the most powerful programming language".split()
+print(s)
