9b022b2064d5cf9095d4d284b6845e0ce6e3e396
prasadhonrao <honrao.prasad@gmail.com>
2020-09-11 12:33:02 +0100

    Add layout page

--- apps/flaskblog/main.py
+++ apps/flaskblog/main.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Prasad Honrao',
+        'title': 'First post',
+        'content': 'This is the first post',
+        'date_posted': '12-NOV-2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Second post',
+        'content': 'This is the second post',
+        'date_posted': '12-NOV-2020'
+    }
+]
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
