19bcd377be8b4051eb5312ddec6c6bce054f7dfb
prasadhonrao <honrao.prasad@gmail.com>
2020-09-29 19:38:34 +0100

    linter updates

--- apps/flaskblog/forms.py
+++ apps/flaskblog/forms.py
@@ -7,7 +7,8 @@ class RegistrationForm(FlaskForm):
     username: StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email: StringField('Email', validators=[DataRequired(), Email()])
     password: PasswordField('Password', validators=[DataRequired()])
-    confirm_password: PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password','Password mismatch')])
+    confirm_password: PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password',
+                                                                                            'Password mismatch')])
     submit: SubmitField('Sign Up')
 
 
